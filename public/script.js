8f496cee868b5e2f482494b90829bf02ce9ed92b
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-13 11:45:44 +0300

    create fetch function , and add event to search input , then create function renderAutoComplete relates #11

--- public/script.js
+++ public/script.js
@@ -1,3 +1,38 @@
-/* eslint-disable no-console */
-/* eslint linebreak-style: ["error", "windows"] */
-console.log('hello');
+const input = document.querySelector(".search-input");
+
+function fetchData(url, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        cb(data);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+input.addEventListener("input", () => {
+  if (input.value === "") {
+    const container = document.getElementsByClassName(
+      "suggestion-container"
+    )[0];
+    container.textContent = "";
+  } else {
+    fetchData(`/displaymovies/${input.value}`, (data) => {
+      renderAutoComplite(data);
+    });
+  }
+});
+
+function renderAutoComplite(data) {
+  const container = document.getElementsByClassName("suggestion-container")[0];
+  container.textContent = "";
+  data.forEach((element) => {
+    let para = document.createElement("p");
+    para.textContent = element;
+    container.appendChild(para);
+  });
+}
